61507d8e7e001081923996127c5ba720ff2e34bb
kartik <kartiksareen18@gmail.com>
2020-03-28 13:12:47 +0530

    ecommerce website

---
+++ routes/admin.js
@@ -0,0 +1,43 @@
+const express=require('express');
+
+const adminController =require('../controllers/admin');
+const isAuth=require('../middleware/is-auth');
+const {body}=require('express-validator/check');
+
+const router=express.Router();
+
+router.get('/add-product',isAuth,adminController.getAddProduct);
+  
+router.post('/add-product',
+    [
+        body('title')
+            .isString()
+            .isLength({min:3})
+            .trim(),
+        body('price').isFloat(),
+        body('description')
+            .isLength({min:5,max:400})
+            .trim()
+    ]
+,isAuth,adminController.postAddProduct);
+
+router.get('/products',isAuth,adminController.getProducts);
+
+router.get('/edit-product/:productId',isAuth,adminController.getEditProduct);
+
+router.post('/edit-product',
+        [
+            body('title')
+                .isString()
+                .isLength({min:3})
+                .trim(),
+            body('price').isFloat(),
+            body('description')
+                .isLength({min:5,max:400})
+                .trim()
+        ]
+,isAuth,adminController.postEditProduct);
+
+router.post('/delete-product',isAuth,adminController.postdeleteProduct);
+
+module.exports=router;
